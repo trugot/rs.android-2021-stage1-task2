0879591cc0b701c8a348e5217ab7fdc623db8b3d
Vova Go <psyghogift@gmail.com>
2021-06-11 18:14:23 +0300

    fix: implement correct functionality in Blocks.kt

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,41 +1,32 @@
 package subtask3
 
 import java.lang.NumberFormatException
+import java.time.DateTimeException
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    private fun sortLocalDataListAndGetString(array: List<Any?>): String {
-        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        val sortedArrayOfDates = array.sortedByDescending {
-            LocalDate.parse(it.toString(), dateTimeFormatter)
-        }
-        val date = LocalDate.parse(sortedArrayOfDates[0].toString())
-        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-        return date.format(formatter)
-    }
-
-
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        val list = blockA.filter { it!!::class.simpleName == blockB.simpleName }
-
         var result: Any
 
-        when (list[0]!!::class.simpleName) {
-            "String" -> {
+        when (blockB) {
+            String::class -> {
+                val list = blockA.filterIsInstance<String>()
                 result = ""
                 for (item in list)
                     result += item
             }
-            "Int" -> {
+            Int::class -> {
+                val list = blockA.filterIsInstance<Int>()
                 result = 0
                 for (item in list)
                     result += item.toString().toInt()
             }
-            "LocalDate" -> {
-                result = sortLocalDataListAndGetString(list)
+            LocalDate::class -> {
+                val date = blockA.filterIsInstance<LocalDate>().max() ?: throw DateTimeException("Wrong Date")
+                val form = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+                result = date.format(form)
             }
             else -> throw NumberFormatException("Something wrong")
         }
