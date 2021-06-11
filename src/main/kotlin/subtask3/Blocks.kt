ac4af3c0c34ea8c9150fd86c2ccdc385009f2acb
Vova Go <psyghogift@gmail.com>
2021-06-11 17:37:21 +0300

    feat: add functionality in Blocks.kt

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,44 @@
 package subtask3
 
+import java.lang.NumberFormatException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
+    private fun sortLocalDataListAndGetString(array: List<Any?>): String {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val sortedArrayOfDates = array.sortedByDescending {
+            LocalDate.parse(it.toString(), dateTimeFormatter)
+        }
+        val date = LocalDate.parse(sortedArrayOfDates[0].toString())
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        return date.format(formatter)
+    }
+
+
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        val list = blockA.filter { it!!::class.simpleName == blockB.simpleName }
+
+        var result: Any
+
+        when (list[0]!!::class.simpleName) {
+            "String" -> {
+                result = ""
+                for (item in list)
+                    result += item
+            }
+            "Int" -> {
+                result = 0
+                for (item in list)
+                    result += item.toString().toInt()
+            }
+            "LocalDate" -> {
+                result = sortLocalDataListAndGetString(list)
+            }
+            else -> throw NumberFormatException("Something wrong")
+        }
+        return result
     }
 }
